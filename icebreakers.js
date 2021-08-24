8ddeb9b1c5b0199b83d0acc680f8085f06aef883
mackenziekbrooks <mackenziekbrooks@gmail.com>
2021-08-24 13:36:24 -0400

    remove dup

--- icebreakers.js
+++
@@ -1,9 +0,0 @@
-var icebreakers = [
-'Favorite breed of dog',
-'Earliest memory of the internet'
-]
-
-function newIcebreaker() {
-	var randomNumber = Math.floor(Math.random() * (icebreakers.length));
-	document.getElementbyID('icebreakerDisplay').innerHTML = icebreakers[randomNumber];
-}
